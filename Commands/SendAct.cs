dbe8956bac769448dc95fe390a11254a4d771b4e
Sharkadi Andrey <sharkadi.a@centrikt.ru>
2016-05-17 22:20:08 +0300

    +

---
+++ Commands/SendAct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtmCliUtility.Commands
+{
+    [CommandProcessorInfo(
+        Name="send-act",
+        Description="Отправляет акт для ТТН",
+        Usage="")]
+    public class SendAct : CommandProcessor
+    {
+        public override string Name
+        {
+            get { return "send-act"; }
+        }
+
+        protected override string ProcessInternal()
+        {
+            return null;
+        }
+    }
+}
