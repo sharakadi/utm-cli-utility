e5435611ead13e64210bd2685a6c3a2af51686fb
Sharkadi Andrey <sharkadi.a@centrikt.ru>
2016-06-01 13:39:58 +0300

    Fix

---
+++ Commands/DocumentVersion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtmCliUtility.Commands
+{
+    [CommandProcessorInfo(
+        Name = "doc-version", 
+        Description ="Управление версиями докуменоборота в ЕГАИС",
+        Usage ="")]
+    public class DocumentVersion :  CommandProcessor
+    {
+        public override string Name
+        {
+            get { return "doc-version"; }
+        }
+
+        protected override string ProcessInternal()
+        {
+            if (Parser.ParameterExists("set"))
+            {
+                var maxVer = Parser.GetValues("set").ToArray();
+                if (!maxVer.Any())
+                    throw new Exception("Следует указать максимальную поддерживаемую версию документооборота.");
+                switch (maxVer.First())
+                {
+                    case "v1":
+                        return ChangeDocVersion("WayBill");
+                    case "v2":
+                        return ChangeDocVersion("WayBill_v2");
+                    default:
+                        throw new Exception("Неизвестная версия документооборота - " + maxVer.First());
+                }
+            }
+            throw new Exception("Параметры не распознаны.");
+        }
+
+        private string ChangeDocVersion(string version)
+        {
+            InfoWriteLineFormat("Переход на версию документооборота: {0}", version);
+        }
+    }
+}
---
+++ DocumentsHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtmCliUtility
+{
+    public static class DocumentsV1012Helper
+    {
+        public static Documents Create(string ownerFsrarId, object item, ItemChoiceType itemChoiceType)
+        {
+            return new Documents()
+            {
+                Document = new DocBody()
+                {
+                    Item = item,
+                    ItemElementName = itemChoiceType
+                },
+                Owner = new SenderInfo()
+                {
+                    FSRAR_ID = ownerFsrarId
+                },
+                Version = "1.0"
+            };
+        }
+    }
+}
